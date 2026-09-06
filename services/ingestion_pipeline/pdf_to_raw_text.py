67cd9974b719eab7bf831f0c90b80fd1e6263e71
Ntsoa Elie <ntsoaelie@gmail.com>
2026-09-06 18:16:03 -0400

    script to read from uploaded pdf

--- services/ingestion_pipeline/pdf_to_raw_text.py
+++ services/ingestion_pipeline/pdf_to_raw_text.py
@@ -1,2 +1,15 @@
-import fitz
+import pymupdf
+
+doc = pymupdf.open("C:\\Users\\airme\\Desktop\\ADM_ProfilDeCompetences.pdf")
+filename = doc.name
+page_count = []
+page_number = 0
+
+for page in doc: 
+    text = page.get_text("text")
+    page_number = page_number + 1
+    current_page = page_number
+    page_count.append((text, current_page, filename))
+    print(text)
+    
 

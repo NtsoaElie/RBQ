b7cc5b0bdaf0a0d27d3ea56b1f8d757ecd28be8b
Ntsoa Elie <ntsoaelie@gmail.com>
2026-09-08 20:38:44 -0400

    made embedding.py into a reusable function

--- services/ingestion_pipeline/embedding.py
+++ services/ingestion_pipeline/embedding.py
@@ -7,8 +7,14 @@ load_dotenv() #load variables from .env file
 my_api_key = os.getenv("OPEN_API_KEY")
 client = OpenAI(api_key=my_api_key)
 
-response = client.embeddings.create(
-    input="The food was delicious and the waiter...", model="text-embedding-3-small"
-)
+# response = client.embeddings.create(
+#     input="The food was delicious and the waiter...", model="text-embedding-3-small"
+# )
 
-print(response.data[0].embedding)
+def embed(my_input):
+    response = client.embeddings.create(
+        input=my_input, model="text-embedding-3-small"
+    )
+    return response.data[0].embedding
+
+# print(response.data[0].embedding)

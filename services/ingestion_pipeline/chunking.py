636269dbb2a7321930ed2478c1f39a0164f003d0
Ntsoa Elie <ntsoaelie@gmail.com>
2026-09-08 20:46:00 -0400

    made chunking.py into a reusable function

--- services/ingestion_pipeline/chunking.py
+++ services/ingestion_pipeline/chunking.py
@@ -1,19 +1,25 @@
 import re
 from pdf_to_raw_text import pdf_to_raw_text
-page_content = pdf_to_raw_text("C:\\Users\\airme\\Desktop\\ADM_ProfilDeCompetences.pdf")
-chunks = []
-current_chunk = ""
 
-for (text, page_number, filename) in page_content:
- lines = text.splitlines()
+# page_content = pdf_to_raw_text("C:\\Users\\airme\\Desktop\\ADM_ProfilDeCompetences.pdf")
 
- for line in lines: 
-     is_top_level = re.search(r'^\d+\.[^\d]', line) #check if top level section
-     if is_top_level: #if its top level that means a new chunk has started
-        chunks.append((current_chunk, page_number, filename)) # we then append the current chunk to chunks[]
-        current_chunk = "" # then we reset current_chunk since top level section means a new chunk has started
-     else: 
-        current_chunk = current_chunk + line #if it's not a top level section that means its sub level or just plain text, we add it to current chunk
-chunks.append((current_chunk, page_number, filename)) # append the current_chunk to chunks[]
-print(len(chunks))
-print(chunks)
+def chunking(page_content):
+   chunks = []
+   current_chunk = ""  
+   for (text, page_number, filename) in page_content:
+         lines = text.splitlines()
+
+         for line in lines: 
+            is_top_level = re.search(r'^\d+\.[^\d]', line) #check if top level section
+            if is_top_level: #if its top level that means a new chunk has started
+               chunks.append((current_chunk, page_number, filename)) # we then append the current chunk to chunks[]
+               current_chunk = "" # then we reset current_chunk since top level section means a new chunk has started
+            else: 
+               current_chunk = current_chunk + line #if it's not a top level section that means its sub level or just plain text, we add it to current chunk
+   chunks.append((current_chunk, page_number, filename)) # append the current_chunk to chunks[]
+   print(len(chunks))
+   print(chunks) 
+   return chunks
+
+
+

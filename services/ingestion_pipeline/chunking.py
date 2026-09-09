c767e8f226caee55e5553e0c142044d9f8a4b660
Ntsoa Elie <ntsoaelie@gmail.com>
2026-09-08 20:18:44 -0400

    added supabase with uv add, added page number and filename to each chunks for meta data purposes

--- services/ingestion_pipeline/chunking.py
+++ services/ingestion_pipeline/chunking.py
@@ -10,10 +10,10 @@ for (text, page_number, filename) in page_content:
  for line in lines: 
      is_top_level = re.search(r'^\d+\.[^\d]', line) #check if top level section
      if is_top_level: #if its top level that means a new chunk has started
-        chunks.append(current_chunk) # we then append the current chunk to chunks[]
+        chunks.append((current_chunk, page_number, filename)) # we then append the current chunk to chunks[]
         current_chunk = "" # then we reset current_chunk since top level section means a new chunk has started
      else: 
         current_chunk = current_chunk + line #if it's not a top level section that means its sub level or just plain text, we add it to current chunk
-chunks.append(current_chunk) # append the current_chunk to chunks[]
+chunks.append((current_chunk, page_number, filename)) # append the current_chunk to chunks[]
 print(len(chunks))
 print(chunks)

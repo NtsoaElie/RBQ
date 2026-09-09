a4bde6125925186c3683a46f596156c3df4bf798
Ntsoa Elie <ntsoaelie@gmail.com>
2026-09-08 21:09:16 -0400

    stored vector store in supabase db

--- services/ingestion_pipeline/vector_store_to_supabase.py
+++ services/ingestion_pipeline/vector_store_to_supabase.py
@@ -1,12 +1,23 @@
 from supabase import create_client, Client
 from dotenv import load_dotenv
+from pdf_to_raw_text import pdf_to_raw_text
+from chunking import chunking
+from embedding import embed
 import os
 
 load_dotenv()
 
-key = os.getenv("SUPABASE_KEY")
+key = os.getenv("SUPABASE_PRIVATE_KEY")
 url = os.getenv("SUPABASE_URL")
 
 supabase: Client = create_client(url, key)
-response = supabase.table("document_chunks").select("*").execute()
-print(response)
+
+chunks = chunking(pdf_to_raw_text("C:\\Users\\airme\\Desktop\\ADM_ProfilDeCompetences.pdf"))
+for (chunk_text, page_number, filename) in chunks:
+    vector = embed(chunk_text)
+    data = supabase.table("document_chunks").insert({
+        "content": chunk_text, "embedding": vector, "filename": filename, "page_number": page_number
+    }).execute()
+    
+
+
